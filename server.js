70449829a6f220561be38232d1bfa4a8288e0e00
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-06 21:44:20 -0500

    didnt push the right files :(

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-  
+ //need to add the sequelize store 
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers'); need to add this
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,5 +23,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log('listenin'));
 });
