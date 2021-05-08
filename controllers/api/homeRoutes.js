2cc3a07163b0feb7d71020d82a11b604d7dee99e
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-08 11:08:05 -0500

    working on routes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,7 +1,7 @@
   
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Home Page
 router.get('/', async (req, res) => {
