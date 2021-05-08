70c8dd7d7f57470a4b0435dfa9d0dcf5b27b649f
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-05-08 13:36:37 -0500

    working on views

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -21,8 +21,8 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
+      posts
+      //logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,22 @@
+
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear()
-      }`;
-    },
-    stringify: (post) => JSON.stringify(post),
-  };
+  formatDate: (date) =>{
+  // Format the date to be MM/DD/YYYY HH:MM:SS
+  let formatted = date.getMonth()+1
+    +"/"+date.getDate()
+    +"/"+date.getFullYear()
+    +" "+date.getHours()+":";
+  if(date.getMinutes() < 10){
+      formatted += "0";
+  }
+  formatted += date.getMinutes()+":";
+  if(date.getSeconds() < 10){
+      formatted += "0";
+  }
+  formatted +=date.getSeconds();
+  return formatted;  
+},
+isAuthor: (myId, authorId)=>{
+  return myId === authorId;
+}
+};
